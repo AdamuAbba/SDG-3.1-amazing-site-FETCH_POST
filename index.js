f3753f4e2b590e3083813a06258793b2241adc85
shy.X <abbaadamu302@gmail.com>
2023-11-09 12:42:32 +0100

    fix: change folder level upload to file level
    we deleted the folder and uploaded it's file contents instead

---
+++ index.js
@@ -0,0 +1,44 @@
+$(document).ready(function () {
+  const loadingText = $("#loadingText");
+  loadingText.hide();
+  loadingText.text("Fetching Posts");
+
+  const form = $("#postForm");
+
+  form.submit(async function (e) {
+    e.preventDefault();
+    const titleValue = $("#titleField").val();
+    const bodyValue = $("#bodyField").val();
+    const responseView = $("#responseView");
+
+    loadingText.show();
+
+    try {
+      const result = await fetch("https://jsonplaceholder.typicode.com/posts", {
+        method: "POST",
+        body: JSON.stringify({
+          title: titleValue,
+          body: bodyValue,
+          userId: 1,
+        }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      });
+
+      loadingText.hide(1000);
+
+      const finalResult = await result.json();
+
+      responseView.html(
+        `<p>id: ${finalResult.id}</p>
+          <p>title: ${finalResult.title}</p>
+          <p>body: ${finalResult.body}</p>
+          <p>userId: ${finalResult.userId}</p>`
+      );
+    } catch (error) {
+      console.log(error);
+      loadingText.hide(1000);
+    }
+  });
+});
